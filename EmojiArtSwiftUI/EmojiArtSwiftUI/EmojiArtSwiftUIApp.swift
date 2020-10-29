ac31851fd65a5be41c36ecc6cd0244de56b0d914
Manson Jones <manson.jones@gmail.com>
2020-10-29 15:26:02 -0700

    Add Xcode project

---
+++ EmojiArtSwiftUI/EmojiArtSwiftUI/EmojiArtSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtSwiftUIApp.swift
+//  EmojiArtSwiftUI
+//
+//  Created by Manson Jones on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
